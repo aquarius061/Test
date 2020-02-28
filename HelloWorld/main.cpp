e11d10d2d969a69b7f40fc23745ac5914a8d598f
aquarius061 <57291937+aquarius061@users.noreply.github.com>
2020-02-28 09:52:52 +0100

    New line
    New line was added

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello fucking world! It's the end of the world!" << endl;
+    cout << "This line was addes after branch was created." << endl;
     return 0;
 }
