d80bd2c2ab0f50a2f36e421c1d17833dd1f26f70
Michal Strzyzewski <michal.strzyzewski@domdata.com>
2020-02-28 09:48:10 +0100

    Change 2
    Adding change 2

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello fucking world! It's the end of the world! Change 1" << endl;
+    cout << "Hello fucking world! It's the end of the world! Change 1! Change 2!" << endl;
     return 0;
 }
